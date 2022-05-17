4eb770fe737bfbb1d261ed9b1741506cdefa6bf0
Johnny Chen <johnnyc1314@gmail.com>
2022-05-17 10:17:20 -0400

    commit tree dab2e2cac95ea6b50fc75149268d6f6493554c2f

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+    !!word.match(/^[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun[a-z]*ing\b/i)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/^[A-Z].*[\W_]\z/)
 end
 
 def valid_phone_number?(phone)
-
+    puts phone
+    !!phone.match(/\d{3}.*\d{3}.*\d{4}/)
 end
